d96026dbf0a10b834f97c26a12446bf0d110f62f
Albert Schweitzer <schweitzer.albert@gmail.com>
2024-05-13 15:40:17 -0400

    hit API server

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <script setup>
+import { ref } from 'vue'
+const availableReads = ['stm', 'opz4', 'pcba']
+const selectedValue = ref('')
+const readResponse = ref('readResponse')
 
+// const API_URL = 'http://localhost:3000'
+
+const requestRead = async function () {
+  console.log('Requesting read for', selectedValue.value)
+  const serviceResponse = await fetch(`${process.env.API_URL}/reading`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ read: selectedValue.value })
+  })
+  const response = await serviceResponse.json()
+  console.log('Read response:', response)
+  readResponse.value = response.reading
+}
 </script>
 
 <template>
-  <div>
+  <div class="text-bold text-red text-xl border border-black">
+    Hello
   </div>
+  <div class="flex flex-col">
+    <select v-model="selectedValue">
+      <option disabled value="">Please select a value type</option>
+      <option v-for="read in availableReads" :key="read" :value="read">{{ read }}</option>
+    </select>
+  </div>
+  <button @click="requestRead">Request Read</button>
+  <div class="pt-4 font-bold text-green-900">{{ readResponse }}</div>
 </template>
 
 <style scoped>
